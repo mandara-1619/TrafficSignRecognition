bfdc29f4eb8f19aaa802d6659a94a35aa6e7f645
mandara-1619 <85288570+mandara-1619@users.noreply.github.com>
2022-07-11 12:47:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,244 @@
-https://github.com/mandara-1619/TrafficSignRecognition.git
+#Mandara Shivakumar
+#Executed in Google Colabaratory
+
+
+! pip install kaggle
+
+! mkdir ~/.kaggle
+
+! cp kaggle.json ~/.kaggle/
+
+! chmod 600 ~/.kaggle/kaggle.json
+
+! kaggle datasets download taejoongyoon/mitbit-arrhythmia-database
+
+! unzip gtsrb-german-traffic-sign
+
+!pip install keras.utils
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import cv2
+import tensorflow as tf
+from PIL import Image
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.utils import to_categorical
+from keras.models import Sequential, load_model
+from keras.layers import Conv2D, MaxPool2D, Dense, Flatten, Dropout
+from tensorflow import keras
+from tensorflow.keras import layers
+
+import os
+from google.colab import drive
+drive.mount('/content/drive', force_remount=True)
+os.chdir('/content')
+
+!pwd
+
+import os
+os.getcwd()
+
+import os
+data =[]
+labels = []
+classes =43 
+cur_path = os.getcwd()
+for i in range(classes):     
+    path = os.path.join(cur_path,'train',str(i))     
+    images = os.listdir(path)
+    for a in images:
+        try:             
+            image = Image.open(path +'/'+ a)             
+            image = image.resize((30,30)) 
+            # Resizing all images into 30*30                                                 
+            image =np.array(image)             
+            data.append(image)             
+            labels.append(i)
+        except Exception as e:
+            print(e)
+
+data = np.array(data) 
+labels = np.array(labels)
+print(data.shape, labels.shape) 
+# (78418,30,30,3)(78418,)
+
+plt.figure(figsize = (12,12))
+for i in range(4) :
+    plt.subplot(1, 4, i+1)
+    plt.imshow(data[i], cmap='gray')
+plt.show()
+
+def prep_dataset(X,y):
+    X_prep = X.astype('float32')
+    y_prep = to_categorical(np.array(y))
+    return (X_prep, y_prep)
+
+X, y = prep_dataset(data,labels)
+
+X_train, X_val, Y_train, Y_val = train_test_split(X,y, test_size=0.2, shuffle=True,stratify=y)
+X_val, X_test, Y_val, Y_test = train_test_split(X_val,Y_val, test_size=0.5, shuffle=True)
+
+model = Sequential() #Sequential Model
+
+#ConvLayer(64 filters) + MaxPooling + BatchNormalization + Dropout
+model.add(layers.Conv2D(filters=32,kernel_size=3,activation='relu',padding='same',input_shape=X.shape[1:]))
+model.add(layers.MaxPool2D(strides=2))
+model.add(layers.BatchNormalization())
+model.add(layers.Dropout(0.3))
+
+#ConvLayer(128 filters) + MaxPooling + BatchNormalization + Dropout
+model.add(layers.Conv2D(filters=128,kernel_size=3,activation='relu',padding='same'))
+model.add(layers.MaxPool2D(strides=2))
+model.add(layers.BatchNormalization())
+model.add(layers.Dropout(0.3))
+
+#ConvLayer(512 filters) + Dropout + ConvLayer(512 filters) + MaxPooling + BatchNormalization
+model.add(layers.Conv2D(filters=512,kernel_size=3,activation='relu',padding='same'))
+model.add(layers.Dropout(0.3))
+model.add(layers.Conv2D(filters=512,kernel_size=3,activation='relu',padding='same'))
+model.add(layers.MaxPool2D(strides=2))
+model.add(layers.BatchNormalization())
+
+#Flatten
+model.add(layers.Flatten())
+
+#2 Dense layers with 4000 hidden units
+model.add(layers.Dense(4000,activation='relu'))
+model.add(layers.Dense(4000,activation='relu'))
+
+#Dense layer with 1000 hidden units
+model.add(layers.Dense(1000,activation='relu'))
+
+#Softmax layer for output
+model.add(layers.Dense(43,activation='softmax'))
+
+model.summary()
+
+model.compile(optimizer='adam',
+             loss='categorical_crossentropy',
+             metrics=['accuracy'])
+
+history= model.fit(X_train,Y_train,
+                 epochs=20,
+                 batch_size=64,
+                 validation_data=(X_val,Y_val))
+
+model.save("./training/TSR.h5")
+
+import seaborn as sns
+
+fig, ax=plt.subplots(2,1,figsize=(12,10))
+fig.suptitle('Train evaluation')
+
+sns.lineplot(ax= ax[0],x=np.arange(0,len(history.history['accuracy'])),y=history.history['accuracy'])
+sns.lineplot(ax= ax[0],x=np.arange(0,len(history.history['accuracy'])),y=history.history['val_accuracy'])
+
+ax[0].legend(['Train','Validation'])
+ax[0].set_title('Accuracy')
+
+sns.lineplot(ax= ax[1],x=np.arange(0,len(history.history['loss'])),y=history.history['loss'])
+sns.lineplot(ax= ax[1],x=np.arange(0,len(history.history['loss'])),y=history.history['val_loss'])
+
+ax[1].legend(['Train','Validation'])
+ax[1].set_title('Loss')
+
+def testing(testcsv):
+    y_test = pd.read_csv(testcsv)
+    label = y_test["ClassId"].values
+    imgs = y_test["Path"].values
+    data=[]
+    for img in imgs:
+        image = Image.open(img)
+        image = image.resize((30,30))
+        data.append(np.array(image))
+    X_test=np.array(data)
+    return X_test,label
+
+X_test, label = testing('Test.csv')
+
+Y_pred = (model.predict(X_test) > 0.5).astype("int64")
+Y_pred
+
+model.save("./training/TSR.h5")
+
+import os
+os.chdir('/content')
+from keras.models import load_model
+model = load_model('./training/TSR.h5')
+
+# Classes of trafic signs
+classes = { 0:'Speed limit (20km/h)',
+            1:'Speed limit (30km/h)', 
+            2:'Speed limit (50km/h)', 
+            3:'Speed limit (60km/h)', 
+            4:'Speed limit (70km/h)', 
+            5:'Speed limit (80km/h)', 
+            6:'End of speed limit (80km/h)', 
+            7:'Speed limit (100km/h)', 
+            8:'Speed limit (120km/h)', 
+            9:'No passing', 
+            10:'No passing veh over 3.5 tons', 
+            11:'Right-of-way at intersection', 
+            12:'Priority road', 
+            13:'Yield', 
+            14:'Stop', 
+            15:'No vehicles', 
+            16:'Veh > 3.5 tons prohibited', 
+            17:'No entry', 
+            18:'General caution', 
+            19:'Dangerous curve left', 
+            20:'Dangerous curve right', 
+            21:'Double curve', 
+            22:'Bumpy road', 
+            23:'Slippery road', 
+            24:'Road narrows on the right', 
+            25:'Road work', 
+            26:'Traffic signals', 
+            27:'Pedestrians', 
+            28:'Children crossing', 
+            29:'Bicycles crossing', 
+            30:'Beware of ice/snow',
+            31:'Wild animals crossing', 
+            32:'End speed + passing limits', 
+            33:'Turn right ahead', 
+            34:'Turn left ahead', 
+            35:'Ahead only', 
+            36:'Go straight or right', 
+            37:'Go straight or left', 
+            38:'Keep right', 
+            39:'Keep left', 
+            40:'Roundabout mandatory', 
+            41:'End of no passing', 
+            42:'End no passing veh > 3.5 tons' }
+
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+def test_on_img(img):
+    data=[]
+    image = Image.open(img)
+    image = image.resize((30,30))
+    data.append(np.array(image))
+    X_test=np.array(data)
+    Y_pred = (model.predict(X_test) > 0.5).astype("int64")
+   
+    
+    return image,Y_pred
+
+import os
+
+cur_path = os.getcwd()
+plot , prediction = test_on_img('/content/Test/00500.png')
+s = [str(i) for i in prediction] 
+print(s)
+print(prediction)
+print(type(prediction))
+
+a = np.where(prediction==1)
+print(a)
+a1=int(a[1])
+print(a1)
+print("Predicted traffic sign is: ", classes[a1])
+plt.imshow(plot)
+plt.show()
